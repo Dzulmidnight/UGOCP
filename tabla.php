9068ed220beb1368871ce45373db96454c080ef6
yasser <yasser.midnight@gmail.com>
2017-08-15 01:48:44 -0500

    Credencial fenam

--- tabla.php
+++ tabla.php
@@ -1,7 +1,7 @@
 			<table  border="0" style="background-image: url('img/fondo_fenam.jpg');padding: 0px;margin: 0px;border: 1px dotted black; border-radius: 16px; font-size:8xp;font-family:arial;color:#1a292c">
 				<tr>
 					<!-- INICIA CARA FRONTAL -->
-					<td style="padding-top:-5px;width:6cm;height:9cm;">
+					<td style="padding-top:-5px;width:6cm;height:10cm;">
 						<table class="otro" style="font-size:10px;">
 							<tr>
 								<td>
@@ -9,18 +9,25 @@
 								</td>
 								<td style="text-align:center;padding-left:40px;padding-top:20px;" rowspan="2">
 									<div>
-										<p>"Por el desarrollo con la equidad de genero"<p>
-										<img src="img/prueba.jpg" style="border: 1px solid #1a292c;border-radius: 10px; width:2cm; height:2.5cm;"/>
-										<p>SECRETARIA GENERAL</p>
+										<p style="margin-left:-20px;color:#e02285;font-size: 12px;">
+											"Por el desarrollo con la equidad de genero"
+										<p>
+										<img src="img/prueba.jpg" style="border-radius: 10px; width:2cm; width: 90px;margin-left: -20px;"/>
+										<p style="color:#550044;font-size: 12px;margin-left: -25px;">
+											SECRETARIA GENERAL
+										</p>
 									</div>
 
 								</td>
 							</tr>
 							<tr>
 								<td style="text-align:center;padding-top:20px;" >
-									<p><i>Ivonne</i></p>
-									<p>Acevedo Palacios</p>
 									<p>
+										<i style="font-size: 14px;"><b>Ivonne</b></i>
+										<br>
+										<span style="font-size: 12px;">Acevedo Palacios</span>
+									</p>
+									<p style="font-size: 11px;">
 										Fraccionamiento lomas de Antequera calle tulipanes #100 Oaxaca de Juárez, Oax, C.P. 6887485
 										<br>
 										Tel. Oficina: 6887485
@@ -30,11 +37,14 @@
 							</tr>
 							<tr>
 								<td>
-									<img style="width: 80px" src="img/circulos.png" alt="">
+									<img style="margin-bottom:-52px;margin-left: -6px;width: 80px" src="img/circulos.png" alt="">
 								</td>
-								<td style="padding-left: 40px;">
-									<p>María Margarita Cabrera Solano</p>
-									<p>Secretario General</p>
+								<td style="padding-left: 8px;padding-top: 90px;">
+									<p style="font-size: 12px;text-align: center">
+										<b>María Margarita Cabrera Solano</b>
+										<br>
+										Secretario General
+									</p>
 								</td>
 							</tr>
 
@@ -44,53 +54,60 @@
 					<!-- TERMINA CARA FRONTAL -->
 
 					<!-- INICIA CARA TRASERA -->
-					<td style="border:3px solid #e13737;width:6cm;height:9cm;">
+					<td style="padding-left:16px;width:6cm;height:10cm;">
 						<table style="font-size:10px;">
 							<tr>
-								<td style="width:50%;text-align:center">
-									'.$detalle['curp'].'
-									<hr style="border:1px solid #2c3e50;width:100%;margin-bottom:0px;margin-top:0px;">
-									<b>CURP</b>
-								</td>
-								<td style="width:50%;text-align:center">
-									'.$detalle['rfc'].'
-									<hr style="border:1px solid #2c3e50;width:100%;margin-bottom:0px;margin-top:0px;">
-									<b>RFC</b>
+								<td colspan="2" style="text-align: center;">
+									<p style="color:#dd0083;font-size: 17px;margin-bottom: 10px;">Datos laborales</p>
 								</td>
 							</tr>
 							<tr>
-								<td style="padding-top:10px;width:50%;text-align:center">
-									<p>'.$detalle['clave_elector'].'</p>
-									<hr style="border:1px solid #2c3e50;width:100%;margin-bottom:0px;margin-top:0px;">
-									<p><b>Clave Elector</b></p>
-								</td>
-								<td style="padding-top:10px;width:50%;text-align:center">
-									<p>'.$detalle['num_ine'].'</p>
-									<hr style="border:1px solid #2c3e50;width:100%;margin-bottom:0px;margin-top:0px;">
-									<p><b>Nº INE</b></p>
-								</td>
-							</tr>
-							<tr>
-								<td style="padding-top:10px;text-align:center" colspan="2">
-									<p>'.$direccion.'</p>
-									<hr style="border:1px solid #2c3e50;width:100%;margin-bottom:0px;margin-top:0px;">
-									<p><b>Domicilio</b></p>
-								</td>
-							</tr>
-							<tr>
-								<td style="padding-top:5px;text-align:center;" colspan="2">
-									<p style="">
-										<img style="" src="../img/firma_ugocp.jpg"></img>
-									</p>
-									<p>Lic. Enrique Jacob González Rojas</p>
-									<hr style="border:1px solid #2c3e50;width:100%;margin-bottom:0px;margin-top:0px;">
+								<td style="font-size: 10px;">
 									<p>
-										Secretario General
+										CLAVE DE ELECTOR: <b>HAYSUE12456743J465</b>
 									</p>
-
+									<p style="margin-bottom: 15px;">
+										CURP: <b>FYDT654356HFG56</b>
+									</p>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2" style="text-align: center;">
+									<p style="color:#dd0083;font-size: 17px;margin-bottom: 10px;">Datos personales</p>
+								</td>
+							</tr>
+							<tr>
+								<td style="font-size: 10px;">
+									<p>
+										Domicilio: <b>Morelos #54B villa de Etla Oaxaca</b>
+									</p>
+									<p>
+										Celular: <b>9512345622</b>
+									</p>
+									<p style="margin-bottom: 15px;">
+										Estado: <b>Oaxaca</b>
+									</p>
 								</td>
 							</tr>
 
+							<tr>
+								<td colspan="2" style="text-align: center;">
+									<p style="background: #ddafe9;color:#dd0083;font-size: 17px;margin-bottom: 10px;border-radius: 10px;padding: 5px;">Datos de emergencia</p>
+								</td>
+							</tr>
+							<tr>
+								<td style="font-size: 10px;">
+									<p>
+										Avisar a: <b>Ernestina Palacios Palestina</b>
+									</p>
+									<p>
+										Telefono: <b>5218021</b>
+									</p>
+									<p>
+										Tipo de sangre: <b>ORH+</b>
+									</p>
+								</td>
+							</tr>
 						</table>
 					</td>
 					<!-- TERMINA CARA TRASERA -->
